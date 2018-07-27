8a2c540987e4d1c260d16a7a35513f506c64c777
Michael Robards <mikerobards@icloud.com>
2018-07-27 09:11:42 -0400

    work on getting clicks accurate

--- script.js
+++ script.js
@@ -1,57 +1,67 @@
 const clickTotal = document.getElementById('clicks');
 const catList = document.getElementById('catList');
 
+// cats object array
 const cats = [{
     name: "Buddy",
-    pic: "assets/Black_barn_cat_-_Public_Domain.jpg"
+    pic: "assets/Black_barn_cat_-_Public_Domain.jpg",
+    count: 0
   },
   {
     name: "Mimi",
-    pic: "assets/cat-face.jpg"
+    pic: "assets/cat-face.jpg",
+    count: 0
   },
   {
     name: "Nelson",
-    pic: "assets/cat3.jpg"
+    pic: "assets/cat3.jpg",
+    count: 0
   },
   {
     name: "Cosmo",
-    pic: "assets/cat4.jpg"
+    pic: "assets/cat4.jpg",
+    count: 0
   },
   {
     name: "Cloudy",
-    pic: "assets/cat5.jpg"
+    pic: "assets/cat5.jpg",
+    count: 0
   }
 ];
 
-cats.forEach(function(cat) {
+// forEach loop creating cat list
+// cats.forEach(function(cat) {
 
+for (let i = 0; i < cats.length; i++) {
+  let cat = cats[i];
+  // creates list of cats with id's
   let li = document.createElement('li');
-
-
   li.innerText = cat.name;
   li.setAttribute("id", cat.name);
 
-  li.addEventListener('click', (function(selectedCat) {
+  // clicking a name on the list displays cat name, pic and # clicks
+  li.addEventListener('click', (function(kitty) {
     let catName = document.getElementById('catName');
     let catPic = document.getElementById('catPic');
-    let catCount = 0;
-    clickTotal.innerText = `${catCount} Clicks!`;
+    console.log(kitty);
+    return function() {
 
-    return function(selectedCat) {
+      catPic.addEventListener('click', function() {
+        console.log(cat);
+        cat.count++;
+        // count++;
+        console.log(cat.count);
+      }, false);
 
-      const cat1 = document.getElementById('catPic');
-      cat1.addEventListener('click', (function(selectedCat) {
-        catCount++;
-        clickTotal.innerText = `${catCount} Clicks!`;
 
-      }), false);
+      // displays clicked selection
       catName.innerText = cat.name;
       catPic.setAttribute('src', cat.pic);
-      let catCount = 0;
-
+      clickTotal.innerText = `${cat.count} Clicks!`;
     };
 
-  })(cat));
+  })());
+
 
   catList.appendChild(li);
-});
+};
