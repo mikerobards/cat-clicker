3291df650cf91d3b01ea2c9f5c1c0babe349c1e6
Michael Robards <mikerobards@icloud.com>
2018-07-27 13:24:54 -0400

    more work on count

--- script.js
+++ script.js
@@ -32,36 +32,39 @@ const cats = [{
 // forEach loop creating cat list
 // cats.forEach(function(cat) {
 
-for (let i = 0; i < cats.length; i++) {
-  let cat = cats[i];
+cats.forEach(function(cat) {
   // creates list of cats with id's
   let li = document.createElement('li');
   li.innerText = cat.name;
   li.setAttribute("id", cat.name);
 
   // clicking a name on the list displays cat name, pic and # clicks
-  li.addEventListener('click', (function(kitty) {
+  li.addEventListener('click', (function() {
     let catName = document.getElementById('catName');
     let catPic = document.getElementById('catPic');
-    console.log(kitty);
+
     return function() {
-
-      catPic.addEventListener('click', function() {
-        console.log(cat);
-        cat.count++;
-        // count++;
-        console.log(cat.count);
-      }, false);
-
-
       // displays clicked selection
       catName.innerText = cat.name;
       catPic.setAttribute('src', cat.pic);
       clickTotal.innerText = `${cat.count} Clicks!`;
+      console.log(cat, cat.count);
+
+      catPic.addEventListener('click', function() {
+        cat.count++;
+        console.log(cat.count);
+      });
     };
 
   })());
-
-
   catList.appendChild(li);
-};
+});
+
+let catCount = 0;
+catPic.addEventListener('click', function() {
+  // console.log(cat);
+  // this.count++;
+  // count++;
+  catCount++;
+  clickTotal.innerText = `${catCount} Clicks!`;
+});
