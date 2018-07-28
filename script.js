dd6f20262da5e62c4168d1a714ab909972e28fc8
Michael Robards <michaelrobards@michaels-imac.attlocal.net>
2018-07-28 16:26:45 -0400

    got count accurate!

---
+++ script.js
@@ -0,0 +1,35 @@
+function Cat(name, pic, count) {
+  this.name = name;
+  this.pic = pic;
+  this.count = count;
+}
+
+
+// const buddy = new Cat("Buddy", "assets/buddy.jpg", 0);
+
+const cats = [{
+    name: "Buddy",
+    pic: "assets/Black_barn_cat_-_Public_Domain.jpg",
+    count: 0
+  },
+  {
+    name: "Mimi",
+    pic: "assets/cat-face.jpg",
+    count: 0
+  },
+  {
+    name: "Nelson",
+    pic: "assets/cat3.jpg",
+    count: 0
+  },
+  {
+    name: "Cosmo",
+    pic: "assets/cat4.jpg",
+    count: 0
+  },
+  {
+    name: "Cloudy",
+    pic: "assets/cat5.jpg",
+    count: 0
+  }
+];
--- scriptV1.js
+++ scriptV1.js
@@ -1,70 +1,75 @@
+// global consts
 const clickTotal = document.getElementById('clicks');
 const catList = document.getElementById('catList');
+const catPic = document.getElementById('catPic');
+
 
 // cats object array
 const cats = [{
     name: "Buddy",
-    pic: "assets/Black_barn_cat_-_Public_Domain.jpg",
+    pic: "assets/buddy.jpg",
     count: 0
   },
   {
     name: "Mimi",
-    pic: "assets/cat-face.jpg",
+    pic: "assets/mimi.jpg",
     count: 0
   },
   {
     name: "Nelson",
-    pic: "assets/cat3.jpg",
+    pic: "assets/nelson.jpg",
     count: 0
   },
   {
     name: "Cosmo",
-    pic: "assets/cat4.jpg",
+    pic: "assets/cosmo.jpg",
     count: 0
   },
   {
     name: "Cloudy",
-    pic: "assets/cat5.jpg",
+    pic: "assets/cloudy.jpg",
+    count: 0
+  },
+  {
+    name: "Cesar",
+    pic: "assets/cesar.jpg",
     count: 0
   }
 ];
 
 // forEach loop creating cat list
-// cats.forEach(function(cat) {
-
 cats.forEach(function(cat) {
+
   // creates list of cats with id's
   let li = document.createElement('li');
   li.innerText = cat.name;
-  li.setAttribute("id", cat.name);
+  li.setAttribute('id', cat.name);
 
   // clicking a name on the list displays cat name, pic and # clicks
   li.addEventListener('click', (function() {
     let catName = document.getElementById('catName');
-    let catPic = document.getElementById('catPic');
 
     return function() {
       // displays clicked selection
       catName.innerText = cat.name;
+      // sets cat's pic
       catPic.setAttribute('src', cat.pic);
+
+      // adds class to pic to identify for click counts
+      catPic.setAttribute('class', cat.name)
+      // displays current count
       clickTotal.innerText = `${cat.count} Clicks!`;
-      console.log(cat, cat.count);
-
-      catPic.addEventListener('click', function() {
-        cat.count++;
-        console.log(cat.count);
-      });
     };
-
   })());
   catList.appendChild(li);
 });
 
-let catCount = 0;
+// adds counts per click to each cat object!
 catPic.addEventListener('click', function() {
-  // console.log(cat);
-  // this.count++;
-  // count++;
-  catCount++;
-  clickTotal.innerText = `${catCount} Clicks!`;
+  let kitty = catPic.className;
+  let index = cats.findIndex(x => x.name == kitty);
+  cats[index].count++;
+  console.log(kitty);
+  console.log(cats[index].count);
+  clickTotal.innerText = `${cats[index].count} Clicks!`;
 });
