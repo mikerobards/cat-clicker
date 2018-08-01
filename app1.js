494cee8413ec15fa5970f07064f1465b689cc7b3
Michael Robards <mikerobards@icloud.com>
2018-08-01 09:01:19 -0400

    got cat display working when cat clicked from list

--- app1.js
+++ app1.js
@@ -5,6 +5,10 @@
 // each other
 
 $(document).ready(function() {
+
+  const catPic = document.getElementById('catPic');
+  const clickTotal = document.getElementById('clicks');
+
   // cats object array
   const cats = [{
       name: "Buddy",
@@ -46,7 +50,22 @@ $(document).ready(function() {
       return catLists;
     },
 
+    showSelectedCat: function(cat) {
+      console.log(cat);
 
+      // displays clicked selection
+      catName.innerText = cat.name;
+
+      // sets cat's pic
+      catPic.setAttribute('src', cat.pic);
+
+      // adds class to pic to identify for click counts
+      catPic.setAttribute('class', cat.name)
+
+      // displays current count
+      clickTotal.innerText = `${cat.count} Clicks!`;
+      return cat;
+    },
 
     // show the selectedCat when a cat in the catList is clicked
 
@@ -70,6 +89,17 @@ $(document).ready(function() {
         let li = document.createElement('li');
         li.innerText = cat.name;
         li.setAttribute('id', cat.name);
+
+        // add event listener to catLists
+        li.addEventListener('click', (function() {
+          let catName = document.getElementById('catName');
+
+          return function() {
+            octopus.showSelectedCat(cat);
+
+          };
+        })());
+
         catList.appendChild(li);
       });
     }
